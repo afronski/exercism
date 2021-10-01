e4cb3d27589bbaa0fb3a58989b3a6b7561caedc8
Wojciech Gawroński <afronski@gmail.com>
2021-10-01 13:50:10 +0200

    Elixir: 4th assignment (2nd solution).

--- elixir/freelancer-rates/lib/freelancer_rates.ex
+++ elixir/freelancer-rates/lib/freelancer_rates.ex
@@ -1,16 +1,20 @@
 defmodule FreelancerRates do
   @working_hours 8.0
-  @working_days 22
+  @billable_days 22
 
-  def daily_rate(hourly_rate), do:
-    hourly_rate * @working_hours
+  def daily_rate(hourly_rate), do: hourly_rate * working_hours
 
-  def apply_discount(before_discount, discount), do:
-    before_discount * ((100 - discount) / 100.0)
+  def apply_discount(before_discount, discount) do
+    before_discount * (100 - discount) / 100 |> Float.round(5)
+  end
 
-  def monthly_rate(hourly_rate, discount), do:
-    trunc(Float.ceil(apply_discount(daily_rate(hourly_rate) * @working_days, discount)))
+  def monthly_rate(hourly_rate, discount) do
+    rate = daily_rate(hourly_rate) * billable_days
+    apply_discount(rate, discount) |> Float.ceil(0) |> trunc
+  end
 
-  def days_in_budget(budget, hourly_rate, discount), do:
-    Float.floor(budget / apply_discount(daily_rate(hourly_rate), discount), 1)
+  def days_in_budget(budget, hourly_rate, discount) do
+    price = daily_rate(hourly_rate) |> apply_discount(discount)
+    budget / price |> Float.floor(1)
+  end
 end
