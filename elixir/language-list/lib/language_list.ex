6a8b5b99015f847299608c9117f8caaff0232cc5
Wojciech Gawroński <afronski@gmail.com>
2021-10-04 15:10:11 +0200

    Elixir: 7th assignment (4th solution).

--- elixir/language-list/lib/language_list.ex
+++ elixir/language-list/lib/language_list.ex
@@ -11,8 +11,5 @@ defmodule LanguageList do
 
   def count(list), do: length(list)
 
-  def exciting_list?(list), do: exciting_list?(list, false)
-
-  defp exciting_list?([], result), do: result
-  defp exciting_list?([head | tail], result), do: exciting_list?(tail, result or head == "Elixir")
+  def exciting_list?(list), do: "Elixir" in list
 end
