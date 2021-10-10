8aec319bacf8c3a71349bef697571bbf91221bd5
Wojciech Gawroński <afronski@gmail.com>
2021-10-10 19:31:02 +0200

    leap/ts (2nd solution)

--- typescript/leap/leap.ts
+++ typescript/leap/leap.ts
@@ -1,14 +1,7 @@
-export function isLeap(year: number) : boolean {
+export function isLeap(year: number = new Date().getFullYear()) : boolean {
   const divisibleBy100 = year % 100 === 0;
   const divisibleBy400 = year % 400 === 0;
+  const divisibleBy4   = year % 4   === 0;
 
-  if (divisibleBy100 && !divisibleBy400) {
-    return false;
-  }
-
-  if (divisibleBy400) {
-    return true;
-  }
-
-  return year % 4 === 0;
+  return (divisibleBy4 && !divisibleBy100) || divisibleBy400;
 }
