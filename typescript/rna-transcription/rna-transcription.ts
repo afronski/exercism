45765d3a7c20618900dae1692ace5f45c62b4185
Wojciech Gawroński <afronski@gmail.com>
2021-10-11 15:07:24 +0200

    rna-transcription/ts (3rd solution)

--- typescript/rna-transcription/rna-transcription.ts
+++ typescript/rna-transcription/rna-transcription.ts
@@ -1,32 +1,29 @@
-const DNA = { 'A': 'A', 'C': 'C', 'G': 'G', 'T': 'T' } as const;
-const RNA = { 'A': 'A', 'C': 'C', 'G': 'G', 'U': 'U' } as const;
+const DNANucleotide = [ 'A', 'C', 'G', 'T' ] as const;
+const RNANucleotide = [ 'A', 'C', 'G', 'U' ] as const;
 
-type DNANucleotide = keyof typeof DNA;
-type RNANucleotide = keyof typeof RNA;
+type DNA = typeof DNANucleotide[number];
+type RNA = typeof RNANucleotide[number];
 
-type DNAStrand = string & { isDNAStrand: true };
-
-const DNA2RNA: Record<DNANucleotide, RNANucleotide> = {
-  G: 'C',
+const DNA2RNA: Record<DNA, RNA> = {
+  A: 'U',
   C: 'G',
+  G: 'C',
   T: 'A',
-  A: 'U'
 };
 
-const notDNANucleotides =
-  (letter: string): letter is DNANucleotide => !DNA.hasOwnProperty(letter)
+const isDNANucleotide =
+  (letter: string): boolean => DNANucleotide.some((nucleotide) => nucleotide === letter)
 
 const isDNAStrand =
-  (strand: string): strand is DNAStrand => strand.split('').filter(notDNANucleotides).length === 0
+  (strand: string): boolean => strand.split('').every(isDNANucleotide)
+
+const toRNAComplement =
+  (letter: string) => DNA2RNA[<DNA> letter]
 
 export const toRna = (input: string): string => {
   if (!isDNAStrand(input)) {
     throw new Error("Invalid input DNA.");
   }
 
-  return input
-          .toLocaleUpperCase()
-          .split('')
-          .map((letter) => DNA2RNA[<DNANucleotide> letter])
-          .join('');
+  return input.toLocaleUpperCase().split('').map(toRNAComplement).join('');
 };
