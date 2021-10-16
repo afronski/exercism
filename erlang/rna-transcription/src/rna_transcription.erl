416ac650add504bf61defa1e3feb6c93fe1461c8
Wojciech Gawroński <afronski@gmail.com>
2021-10-16 16:51:36 +0200

    rna-transcription/erlang (2nd solution)

--- erlang/rna-transcription/src/rna_transcription.erl
+++ erlang/rna-transcription/src/rna_transcription.erl
@@ -2,10 +2,11 @@
 
 -export([to_rna/1]).
 
-
+-spec dna2rna(char()) -> char().
 dna2rna($G) -> $C;
 dna2rna($C) -> $G;
 dna2rna($T) -> $A;
 dna2rna($A) -> $U.
 
+-spec to_rna(string()) -> string().
 to_rna(Strand) -> lists:map(fun dna2rna/1, string:uppercase(Strand)).
