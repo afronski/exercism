267e91ede89eb7b42b46d409cec5cd0d391e95ed
Wojciech Gawroński <afronski@gmail.com>
2021-10-17 12:12:47 +0200

    assembly-line/rust (2nd solution)

--- rust/assembly-line/src/lib.rs
+++ rust/assembly-line/src/lib.rs
@@ -1,29 +1,22 @@
-const BASE_PRODUCTION_RATE: u32 = 221;
-
-const SLOW: std::ops::RangeInclusive<u8> = 1..=4;
-const FASTER: std::ops::RangeInclusive<u8> = 5..=8;
-const FASTEST: std::ops::RangeInclusive<u8> = 9..=10;
+const BASE_PRODUCTION_RATE: f64 = 221.0;
 
 fn success_rate(speed: u8) -> f64 {
-    if SLOW.contains(&speed) {
-        return 1.0;
-    } else if FASTER.contains(&speed) {
-        return 0.9;
-    } else if FASTEST.contains(&speed) {
-        return 0.77;
-    } else {
-        return 0.0;
+    match speed {
+        0..=4 => 1.0,
+        5..=8 => 0.9,
+        9..=10 => 0.77,
+        _ => panic!("Unexpected speed: {}", speed)
     }
 }
 
 pub fn production_rate_per_hour(speed: u8) -> f64 {
-    let base: f64 = (BASE_PRODUCTION_RATE * speed as u32) as f64;
+    let base: f64 = BASE_PRODUCTION_RATE * (speed as f64);
 
     return base * success_rate(speed);
 }
 
 pub fn working_items_per_minute(speed: u8) -> u32 {
-    let per_hour = production_rate_per_hour(speed);
+    let per_hour: u32 = production_rate_per_hour(speed) as u32;
 
-    return (per_hour / 60.0).floor() as u32;
+    return per_hour / 60;
 }
