e79f9e01c0cfb35ed3d1fc4e69bed88c1250f467
Wojciech Gawroński <afronski@gmail.com>
2021-10-25 16:00:58 +0200

    high-score/elixir (3rd solution)

--- elixir/high-score/lib/high_score.ex
+++ elixir/high-score/lib/high_score.ex
@@ -1,13 +1,13 @@
 defmodule HighScore do
-  @starting_score 0
+  @initial_score 0
 
   def new(), do: %{}
 
-  def add_player(scores, name, score \\ @starting_score), do: Map.put(scores, name, score)
+  def add_player(scores, name, score \\ @initial_score), do: Map.put(scores, name, score)
   def remove_player(scores, name), do: Map.delete(scores, name)
   def get_players(scores), do: Map.keys(scores)
 
-  def reset_score(scores, name), do: Map.put(scores, name, @starting_score)
+  def reset_score(scores, name), do: Map.put(scores, name, @initial_score)
 
-  def update_score(scores, name, score), do: Map.update(scores, name, score, fn(old) -> old + score end)
+  def update_score(scores, name, score), do: Map.update(scores, name, score, &(&1 + score))
 end
