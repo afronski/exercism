93f774d6c9649a4e472de1480e77421da132ea46
Wojciech Gawroński <afronski@gmail.com>
2021-10-27 20:19:00 +0200

    phone-number/r (3rd solution)

--- r/phone-number/phone-number.R
+++ r/phone-number/phone-number.R
@@ -1,10 +1,10 @@
 parse_phone_number <- function(number_string) {
-  regex <- '^\\s*\\+?1?\\(?([2-9][0-9]{2})\\)?\\s*\\.?([2-9][0-9]{2})\\-?\\.?\\s*([0-9]{4})\\s*$'
-  number <- gsub(regex, '\\1\\2\\3', number_string)
+  all_nums <- gsub('\\D', '', number_string)
 
-  if (nchar(number) != 10) {
+  pattern <- '^1?[2-9]\\d{2}[2-9]\\d{6}$'
+  if (!grepl(pattern, all_nums)) {
     return(NULL)
   }
 
-  number
+  gsub('^1', '', all_nums)
 }
