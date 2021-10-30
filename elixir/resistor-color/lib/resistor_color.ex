50b97c6069b1d70b18b4dc0a667d6a903c1df673
Wojciech Gawroński <afronski@gmail.com>
2021-10-30 09:07:13 +0200

    elixir/resistor-color (2nd solution)

--- elixir/resistor-color/lib/resistor_color.ex
+++ elixir/resistor-color/lib/resistor_color.ex
@@ -1,16 +1,11 @@
 defmodule ResistorColor do
-  @doc """
-  Return the value of a color band
-  """
-  @spec code(atom) :: integer()
-  def code(:black), do: 0
-  def code(:brown), do: 1
-  def code(:red), do: 2
-  def code(:orange), do: 3
-  def code(:yellow), do: 4
-  def code(:green), do: 5
-  def code(:blue), do: 6
-  def code(:violet), do: 7
-  def code(:grey), do: 8
-  def code(:white), do: 9
+  @moduledoc false
+
+  @colors ~w(black brown red orange yellow green blue violet grey white)
+
+  @spec colors() :: list(String.t())
+  def colors, do: @colors
+
+  @spec code(String.t()) :: integer()
+  def code(color), do: Enum.find_index(@colors, fn c -> c == color end)
 end
