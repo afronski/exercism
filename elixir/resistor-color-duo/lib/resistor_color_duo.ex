b83dd7ca091bd9cdf460ac876d9fa680f049474e
Wojciech Gawroński <afronski@gmail.com>
2021-11-02 19:50:07 +0100

    resistor-color-duo/elixir (2nd solution)

--- elixir/resistor-color-duo/lib/resistor_color_duo.ex
+++ elixir/resistor-color-duo/lib/resistor_color_duo.ex
@@ -3,18 +3,19 @@ defmodule ResistorColorDuo do
   Helping with color-coded resistors.
   """
 
-  @colors ~w(black brown red orange yellow green blue violet grey white)a
+  @resistance_chart %{
+    :black => 0, :brown => 1, :red => 2, :orange => 3, :yellow => 4, :green => 5, :blue => 6, :violet => 7,
+    :grey => 8, :white => 9
+ }
 
   @doc """
   Calculate a resistance value from two colors
   """
   @spec value(colors :: [atom]) :: integer
   def value(colors) do
-    String.to_integer("#{get_color(Enum.at(colors, 0))}#{get_color(Enum.at(colors, 1))}")
-  end
-
-  @spec get_color(color :: atom) :: integer
-  defp get_color(color) do
-    Enum.find_index(@colors, fn c -> c == color end)
+    colors
+    |> Enum.map(&(@resistance_chart[&1]))
+    |> Enum.take(2)
+    |> Integer.undigits
   end
 end
