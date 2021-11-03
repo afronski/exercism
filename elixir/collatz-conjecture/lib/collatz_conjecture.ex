1e39804b73d50f0e28c03144d50cbb8300b735a9
Wojciech Gawroński <afronski@gmail.com>
2021-11-03 16:08:35 +0100

    collatz-conjecture/elixir (1st solution)

---
+++ elixir/collatz-conjecture/lib/collatz_conjecture.ex
@@ -0,0 +1,15 @@
+defmodule CollatzConjecture do
+  @doc """
+  calc/1 takes an integer and returns the number of steps required to get the
+  number to 1 when following the rules:
+    - if number is odd, multiply with 3 and add 1
+    - if number is even, divide by 2
+  """
+  @spec calc(input :: pos_integer()) :: non_neg_integer()
+  def calc(input) when input > 0, do: calc(input, 0)
+
+  @spec calc(number :: pos_integer(), steps :: non_neg_integer()) :: non_neg_integer()
+  def calc(1, steps), do: steps
+  def calc(number, steps) when rem(number, 2) == 0, do: calc(div(number, 2), steps + 1)
+  def calc(number, steps) when rem(number, 2) != 0, do: calc(3 * number + 1, steps + 1)
+end
