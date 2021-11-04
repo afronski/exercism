080c283ac1baf52d0e766abd3ebc579672540b4d
Wojciech Gawroński <afronski@gmail.com>
2021-11-04 21:49:50 +0100

    word-count/r (2nd solution)

--- r/word-count/word-count.R
+++ r/word-count/word-count.R
@@ -1,4 +1,3 @@
 word_count <- function(input) {
-  sanitized <- trimws(gsub('\\s{2,}', ' ', gsub('[^ \'a-z0-9]', '', tolower(input))))
-  as.list(table(unlist(strsplit(sanitized,' '))))
+  as.list(table(strsplit(tolower(trimws(input)), '\\W+')))
 }
