805a9056f73cb89675123deee5443613a22267b4
Wojciech Gawroński <afronski@gmail.com>
2021-11-11 16:54:15 +0100

    matrix/ts (2nd solution)

--- typescript/matrix/matrix.ts
+++ typescript/matrix/matrix.ts
@@ -3,24 +3,14 @@ export class Matrix {
   public readonly columns: number[][];
 
   constructor(payload: string) {
-    this.rows =
-      payload
-        .split('\n')
-        .map(
-          (line) => line.split(' ')
-                               .map((numAsStr) => parseInt(numAsStr, 10))
-        );
+    this.rows = payload
+      .split('\n')
+      .map((line) => line.split(' ').map((numAsStr) => parseInt(numAsStr, 10)));
 
     this.columns = [];
 
     for (let i = 0; i < this.rows.length; ++i) {
-      for (let j = 0; j < this.rows[i].length; ++j) {
-        if (typeof(this.columns[j]) === 'undefined') {
-          this.columns[j] = [];
-        }
-
-        this.columns[j][i] = this.rows[i][j];
-      }
+      this.columns.push(this.rows.map(row => row[i]));
     }
   }
 }
