efb185b8dd402442a76a3251be56d42854072dfd
Wojciech Gawroński <afronski@gmail.com>
2021-11-15 09:19:12 +0100

    ghost-gobble-arcade-game/python (1st solution)

---
+++ python/ghost-gobble-arcade-game/arcade_game.py
@@ -0,0 +1,47 @@
+"""
+Module that handles rules from Pac-Man.
+"""
+
+def eat_ghost(power_pellet_active, touching_ghost):
+    """
+    Can Pac-Man eat a ghost?
+
+    :param power_pellet_active: bool - does the player have an active power pellet?
+    :param touching_ghost:  bool - is the player touching a ghost?
+    :return: bool
+    """
+    return power_pellet_active and touching_ghost
+
+
+def score(touching_power_pellet, touching_dot):
+    """
+    Should we increase score for Pac-Man?
+
+    :param touching_power_pellet: bool - does the player have an active power pellet?
+    :param touching_dot:  bool - is the player touching a dot?
+    :return: bool
+    """
+    return touching_power_pellet or touching_dot
+
+
+def lose(power_pellet_active, touching_ghost):
+    """
+    Is Pac-Man losing?
+
+    :param power_pellet_active: bool - does the player have an active power pellet?
+    :param touching_ghost: bool - is the player touching a ghost?
+    :return: bool
+    """
+    return not power_pellet_active and touching_ghost
+
+
+def win(has_eaten_all_dots, power_pellet_active, touching_ghost):
+    """
+    Has Pac-man won?
+
+    :param has_eaten_all_dots: bool - has the player "eaten" all the dots?
+    :param power_pellet_active: bool - does the player have an active power pellet?
+    :param touching_ghost:  bool - is the player touching a ghost?
+    :return: bool
+    """
+    return has_eaten_all_dots and not (not power_pellet_active and touching_ghost)
