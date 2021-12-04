2ddf6ab6fbba24901230531726fc7c8ff945ea70
Wojciech Gawroński <afronski@gmail.com>
2021-12-04 12:59:15 +0100

    nucleotide-count/julia (2nd solution)

--- julia/nucleotide-count/nucleotide-count.jl
+++ julia/nucleotide-count/nucleotide-count.jl
@@ -6,17 +6,15 @@ The count of each nucleotide within `strand` as a dictionary.
 Invalid strands raise a `DomainError`.
 
 """
-function count_nucleotides(strand)
-  start = Dict('A' => 0, 'C' => 0, 'G' => 0, 'T' => 0)
+function count_nucleotides(strand::AbstractString)
+    nucleotides = "ACGT"
 
-  function incrementNucleotide(dictionary, nucleotide)
-    if !(nucleotide in ['A', 'C', 'G', 'T'])
-      throw(DomainError(strand, "Unexpected Nucleotide Provided in the Strand"))
+    counts = Dict{Char, Integer}((n, 0) for n in nucleotides)
+
+    for c in strand
+        c ∈ nucleotides || throw(DomainError("$(c) is not a valid nucleotide."))
+        counts[c] += 1
     end
 
-    dictionary[nucleotide] += 1
-    return dictionary
-  end
-
-  reduce(incrementNucleotide, collect(strand), init = start)
+    counts
 end
