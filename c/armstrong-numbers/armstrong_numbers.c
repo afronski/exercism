20b7d3917f065f263ed6ce2c412519daf8cd29f4
Wojciech Gawroński <afronski@gmail.com>
2021-12-08 13:22:30 +0100

    armstrong-numbers/c (2nd solution)

--- c/armstrong-numbers/armstrong_numbers.c
+++ c/armstrong-numbers/armstrong_numbers.c
@@ -3,17 +3,16 @@
 #include "armstrong_numbers.h"
 
 bool is_armstrong_number(int candidate) {
-  int remainder, originalNum, n = 0;
-  float result = 0.0;
+  int exponent, final = candidate;
+  int sum = 0;
 
-  for (originalNum = candidate; originalNum != 0; ++n) {
-    originalNum /= 10;
+  exponent = floor(log10(candidate) + 1);
+
+  while (candidate > 0) {
+    int digit = candidate % 10;
+    sum = sum + pow(digit, exponent);
+    candidate /= 10;
   }
 
-  for (originalNum = candidate; originalNum != 0; originalNum /= 10) {
-    remainder = originalNum % 10;
-    result += pow(remainder, n);
-  }
-
-  return ((int) result) == candidate;
+  return sum == final;
 }
