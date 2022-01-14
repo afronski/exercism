4c4ea620e065898353dfabeaede516546f1c691a
Wojciech Gawroński <afronski@gmail.com>
2022-01-14 19:30:38 +0100

    fsharp/booking-up-for-beauty (1st solution).

---
+++ fsharp/booking-up-for-beauty/BookingUpForBeauty.fs
@@ -0,0 +1,18 @@
+module BookingUpForBeauty
+
+open System
+
+let schedule (appointmentDateDescription: string): DateTime =
+    Convert.ToDateTime(appointmentDateDescription)
+
+let hasPassed (appointmentDate: DateTime): bool =
+    appointmentDate < DateTime.Now
+
+let isAfternoonAppointment (appointmentDate: DateTime): bool =
+    appointmentDate.Hour >= 12 && appointmentDate.Hour < 18
+
+let description (appointmentDate: DateTime): string =
+    sprintf "You have an appointment on %s." <| appointmentDate.ToString "M/d/yyyy h:mm:ss tt"
+
+let anniversaryDate(): DateTime =
+    DateTime(DateTime.Now.Year, 9, 15)
