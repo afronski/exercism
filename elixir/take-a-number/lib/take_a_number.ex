c4a895364fee2890c6c527c5831ea919bc380784
Wojciech Gawroński <afronski@gmail.com>
2022-07-18 21:32:39 +0200

    elixir/take-a-number (1st solution).

---
+++ elixir/take-a-number/lib/take_a_number.ex
@@ -0,0 +1,28 @@
+defmodule TakeANumber do
+  @initial_state 0
+
+  @state_key :ticket_number
+
+  def start(), do: spawn(&handler/0)
+
+  defp handler do
+    receive do
+      {:report_state, sender_pid} ->
+        send(sender_pid, get_ticket())
+
+      {:take_a_number, sender_pid} ->
+        next_ticket = get_ticket() + 1
+        send(sender_pid, next_ticket)
+        Process.put(@state_key, next_ticket)
+
+      :stop ->
+        exit(:stop)
+
+      _ -> nil
+    end
+
+    handler()
+  end
+
+  defp get_ticket, do: Process.get(@state_key, @initial_state)
+end
