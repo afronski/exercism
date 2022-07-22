b72754fcf10f687de838e879c2452522af6174ff
Wojciech Gawroński <afronski@gmail.com>
2022-07-22 15:15:09 +0200

    elixir/basketball-website (1st solution).

---
+++ elixir/basketball-website/lib/basketball_website.ex
@@ -0,0 +1,4 @@
+defmodule BasketballWebsite do
+  def extract_from_path(data, path), do: Enum.reduce(String.split(path, "."), data, fn key, acc -> acc[key] end)
+  def get_in_path(data, path), do: get_in(data, String.split(path, "."))
+end
